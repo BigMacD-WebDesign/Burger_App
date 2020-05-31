7de0f0938553b9129a254c5b423c84e674fce557
Scott MacDonald <scottmacd88@yahoo.com>
2020-05-31 10:10:44 -0700

    Added CSS. Final Push.

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,3 @@
-// var orm = require("../config/orm.js");
-
-// var burgers = {
-//   all: function(cb) {
-//       console.log("Burgers");
-//     orm.all("burgers", function(res) {
-//         console.log(res);
-//       cb(res);
-//     });
-//   },
-
-//   create: function(name, cb) {
-//     orm.create("burgers", [
-//       "burger_name", "devoured"
-//     ], [
-//       name, false
-//     ], cb);
-//   },
-//   update: function(id, cb) {
-//     var condition = "id=" + id;
-//     orm.update("burgers", {
-//       devoured: true
-//     }, condition, cb);
-//   }
-// };
 
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
